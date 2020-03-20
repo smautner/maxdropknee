573d2dc1ed76d12fad4707209b2dfd89ffd92791
S Mautner <mautner@cs.uni-freiburg.de>
2020-03-20 15:32:23 +0100

    classic method and smoothed method

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # maxdropknee
 
-
-when reading about knee detection the example curves look more smooth than in real life.
-So i write my own knee detection
+implementation of angle based knee point detection
--- maxdropknee/__init__.py
+++ maxdropknee/__init__.py
@@ -1,34 +1,70 @@
 import numpy as np
-def maxdropknee(values):  
+import math
 
-    # every value gets a score, 
-    # if it goes up: nextvalue-v
-    # if it goes down: nextminimum - value
-    # return minimum of scores
+
+def angle(b,c):
+    tan = math.degrees(math.atan(abs(c-b)))
+    if c>b: return 90+tan
+    return 90-tan
+
+def angles(a,b,c):
+    r1=angle(b,a)
+    r2=angle(b,c)
+    return r1+r2
+
+def maxdropknee(values, smooth=3):  
+    """ 
+       avg left+right -> minumize angle 
+    """
+    valz = [ np.mean(values[i:i+smooth]) for i in range(len(values)-smooth)]
     
     scores = []
-    for i,v in enumerate(values[:-1]):
-        if values[i+1] > v:
-            scores.append(values[i+1]-v)
-        else:
-            rest = values[i+1:]
-            if rest:
-                cur  =  v
-                for e in rest:
-                    if e < cur: # next value is smaler. continue
-                        cur = e
-                    else: # next value is larger, break
-                        break 
-                scores.append(cur-v)
+    for i,v in enumerate(values[smooth:-smooth-1]):
+        i+=smooth
+        x= valz[i-smooth]
+        z=valz[i+1]
+        ang = angles(x,v,z)
+        scores.append(ang) 
+        
+    return np.argmin(scores+smooth)
 
-    '''
-    from matplotlib import pyplot as plt
-    res=[0]*len(values)
-    res[np.argmin(scores)]= -2000
-    plt.plot(values)
-    plt.plot(scores)
-    plt.plot(res)
-    plt.show()
-    '''
-    return np.argmin(scores)
+
+
+def paper_angle_based(values):
+    # INITIALIZE
+    cur,pre,aft = [values[0]]*3
+
+    # GET DIFFs
+    diffs = []
+    for i,m in enumerate(range(cmin, cmax)):
+        cur = values[i]
+        aft = values[i+1]
+        diffs.append((i, m, pre+aft-2*cur ))
+        pre = cur
+
+    # find local minima in diff function
+    locmin = [ v for i,v in enumerate(diffs[:-1])
+            if diffs[i][2] < diffs[i-1][2] and diffs[i][2] < diffs[i+1][2] ]
+
+
+    # for each n with decreasing order of localmin value ( is this bic or diff?)
+    locmin.sort(key= lambda x: x[2], reverse=True) # decreasing order of locmin
+
+    cur = -999999999
+    lastm = -1
+    their_angle = lambda a,b,c: math.atan(1/abs(b-a)) + math.atan(1/abs(c-b))
+    for i,m,v in locmin:
+        a = their_angle(*values[i-1:i+2])
+
+        if a < cur:
+            break
+        else:
+            cur = a
+            lastm = m
+    else:
+        print("something went wrong")
+    
+    return lastm
+
+
    
